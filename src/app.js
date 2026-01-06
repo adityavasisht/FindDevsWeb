fa6293cf2d27dde2b5cfa33cb861d48681f57dca
Aditya <vasishtaditya8@gmail.com>
2026-01-06 15:11:19 +0530

    scsc

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use("/",(req,res)=>{
+app.use("/hello",(req,res,next)=>{
+    next();
     res.send("hello world");
-})
+    
+},(req,res)=>{
+    res.send("2nd response");
+    next();
+},
+(req,res)=>{
+    res.send("3rd response");
+}
+);
 
 app.listen(3000,()=>{
     console.log("server running on the port 3000...");
