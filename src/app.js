792bbc0348b8c1409f0ed8a0a3ceb55267e6d0a1
Aditya <vasishtaditya8@gmail.com>
2026-01-11 11:50:37 +0530

    dkckdz

--- src/app.js
+++ src/app.js
@@ -2,16 +2,23 @@ const express = require('express');
 const app = express();
 const {authAdmin,authUser} = require("./middlewares/auth")
 const { connectDB} = require("./config/database");
-const userisntance = require('./models/user.js')
+const User = require('./models/user.js')
 
 
 
 app.use(express.json());
+
+app.get("/userInfo", async(req,res)=>{
+    const email = req.body.emailId;
+    const user = await User.find ({emailId : email});
+    res.send(user);
+
+})
 app.post("/signup",async(req,res)=>{
     
 
     try {
-        const user = new userisntance(req.body);
+        const user = new User(req.body);
 
     console.log(user);
         await user.save();
