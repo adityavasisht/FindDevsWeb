29d828d7170003e23a567a5c5b890197c4c510eb
Aditya Vasisht <107064680+adityavasisht@users.noreply.github.com>
2026-01-27 09:30:47 +0530

    Delete src directory

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const connectDB  = async()=>{
- await mongoose.connect('mongodb+srv://aditya009:Helloaadi007@cluster0.xu7dt1v.mongodb.net/DevTinder');
-};
-
-
-module.exports = {
-    connectDB
-}
